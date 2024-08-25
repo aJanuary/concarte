1991405bae5e79af3ddfcae1bb7de907c9d22bd1
Andrew January <ajanuary@evertz.com>
2024-08-25 13:23:11 +0100

    Add basic search functionality
    We could do with some better fuzzy matching, and some keyboard controls
    like down/up navigating the list. But the basics work.

--- app/RoomSelect.tsx
+++ app/RoomSelect.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import { Room, Config } from './common_types';
 
 interface RoomSelectProps {
@@ -15,21 +14,31 @@ export default function RoomSelect({ config, onRoomSelected }: RoomSelectProps) 
       setFocused(false);
     }
   };
+  const [query, setQuery] = React.useState('');
 
   const onRoomClick = (room: Room) => {
     setFocused(false);
+    setQuery('');
     onRoomSelected && onRoomSelected(room);
   };
 
-  // TODO: Implement a search function
+  let results;
+  if (query === '') {
+    results = config.levels.flatMap(level => level.rooms);
+  } else {
+    results = config.levels.flatMap(level => level.rooms).filter(room => {
+      return room.label.toLowerCase().includes(query.toLowerCase()) ||
+        room.aliases?.some(alias => alias.toLowerCase().includes(query.toLowerCase()));
+    });
+  }
 
   return <div className={`transition absolute top-0 w-screen ${focused ? 'h-screen bg-white' : 'bg-transparent'}`}  onFocus={onFocus} onClick={onDismiss} >
     <div className="px-4 py-2">
-      <input className="p-2 pl-12 border rounded-full w-full" tabIndex={2} type="text" placeholder="Search for a room..."/>
+      <input className="p-2 pl-12 border rounded-full w-full" tabIndex={2} type="text" placeholder="Search for a room..." value={query} onChange={e => setQuery(e.target.value)}/>
       { focused && <button className="absolute top-2 left-6 pl-1 pr-2 text-3xl cursor-pointer text-slate-700" tabIndex={1} onClick={onDismiss}>&lt;</button>}
     </div>
     <ul className={`px-4 py-2 ${!focused && 'hidden'}`}>
-      {config.levels.map(level => level.rooms.map((room, i) => {
+      {results.map(((room, i) => {
         return <li key={room.id}>
           <a className="block p-2 border-b-2 border-slate-100 cursor-pointer hover:bg-slate-100" href={`/${room.id}`} tabIndex={i+3} onClick={e => { e.preventDefault(); onRoomClick(room); }}>
             <p>{room.label}</p>
