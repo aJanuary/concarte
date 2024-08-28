f93214c0624e43063c1cfa307938a3e348e2da17
Andrew January <ajanuary@evertz.com>
2024-08-28 19:28:34 +0100

    Add icons to room select
    Replace the < with a chevron icon, and add a magnifying glass when it is
    not selected.

--- app/RoomSelect.tsx
+++ app/RoomSelect.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Room, Config } from './common_types';
+import { ChevronLeftIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
 interface RoomSelectProps {
   config: Config;
@@ -10,7 +11,7 @@ export default function RoomSelect({ config, onRoomSelected }: RoomSelectProps) 
   const [focused, setFocused] = React.useState(false);
   const onFocus = () => setFocused(true);
   const onDismiss = (e: React.MouseEvent<HTMLElement>) => {
-    if (e.target === e.currentTarget) {
+    if (e.target === e.currentTarget || e.currentTarget.tagName === 'BUTTON') {
       setFocused(false);
     }
   };
@@ -32,10 +33,19 @@ export default function RoomSelect({ config, onRoomSelected }: RoomSelectProps) 
     }).sort((a, b) => a.label.localeCompare(b.label));
   }
 
+  let icon;
+  if (focused) {
+    icon = <button className="absolute top-2 left-6 pl-1 pr-2 pt-2 pb-2 cursor-pointer text-primary-text" tabIndex={1} onClick={onDismiss}>
+    <ChevronLeftIcon className="size-6" />
+  </button>
+  } else {
+    icon = <MagnifyingGlassIcon className="absolute size-10 top-2 left-6 pl-1 pr-2 pt-2 pb-2 text-primary-text" />
+  }
+
   return <div className={`transition absolute top-0 w-screen ${focused ? 'h-screen bg-background' : 'bg-transparent'}`}  onFocus={onFocus} onClick={onDismiss} >
     <div className="px-4 py-2">
       <input className="p-2 pl-12 border rounded-full w-full border-border bg-background text-primary-text placeholder-secondary-text" tabIndex={2} type="text" placeholder="Search for a room..." value={query} onChange={e => setQuery(e.target.value)}/>
-      { focused && <button className="absolute top-2 left-6 pl-1 pr-2 text-3xl cursor-pointer text-primary-text" tabIndex={1} onClick={onDismiss}>&lt;</button>}
+      { icon }
     </div>
     <ul className={`absolute top-14 right-0 bottom-0 left-0 px-4 py-2 overflow-y-auto ${!focused && 'hidden'}`}>
       {results.map(((room, i) => {
