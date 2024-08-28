5b63e0ce550c64335c05f4b9ccd9a0840b8fb09a
Andrew January <ajanuary@evertz.com>
2024-08-28 19:42:46 +0100

    Make room select appear on top of info panel

--- app/RoomSelect.tsx
+++ app/RoomSelect.tsx
@@ -42,12 +42,12 @@ export default function RoomSelect({ config, onRoomSelected }: RoomSelectProps) 
     icon = <MagnifyingGlassIcon className="absolute size-10 top-2 left-6 pl-1 pr-2 pt-2 pb-2 text-primary-text" />
   }
 
-  return <div className={`transition absolute top-0 w-screen ${focused ? 'h-screen bg-background' : 'bg-transparent'}`}  onFocus={onFocus} onClick={onDismiss} >
+  return <div className={`transition absolute top-0 w-screen z-50 ${focused ? 'h-screen bg-background' : 'bg-transparent'}`}  onFocus={onFocus} onClick={onDismiss} >
     <div className="px-4 py-2">
       <input className="p-2 pl-12 border rounded-full w-full border-border bg-background text-primary-text placeholder-secondary-text" tabIndex={2} type="text" placeholder="Search for a room..." value={query} onChange={e => setQuery(e.target.value)}/>
       { icon }
     </div>
-    <ul className={`absolute top-14 right-0 bottom-0 left-0 px-4 py-2 overflow-y-auto ${!focused && 'hidden'}`}>
+    <ul className={`absolute top-14 right-0 bottom-0 left-0 px-4 py-2 overflow-y-auto ${focused ? '' : 'hidden'}`}>
       {results.map(((room, i) => {
         return <li key={room.id}>
           <a className="block p-2 border-b-2 border-border cursor-pointer hover:bg-highlight-background" href={`/${room.id}`} tabIndex={i+3} onClick={e => { e.preventDefault(); onRoomClick(room); }}>
