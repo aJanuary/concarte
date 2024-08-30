0159083fd34d46dae2bb0d11366b527eabd7c32b
Andrew January <ajanuary@evertz.com>
2024-08-30 19:10:39 +0100

    Remap headings in info panel to start at h4
    This reflects the correct information hierarchy, and happens to look
    better with the default stayling from the typography plugin.

--- app/InfoPanel.tsx
+++ app/InfoPanel.tsx
@@ -27,9 +27,19 @@ export default function InfoPanel({ room, expanded, onInfoPanelExpandChange }: I
   return <div className="absolute bottom-0 left-0 right-0 p-4 pt-6 shadow-top bg-background">
     <header className={`${(expanded && room.description) ? 'pb-2 border-border border-1 border-b' : ''} ${room.description ? 'cursor-pointer' : ''}`} onClick={onPanelClick}>
       <p className="absolute top-0 left-0 right-0">{ icon }</p>
-      <h2 className="text-xl">{room.label}</h2>
-      <h3 className="text-secondary-text">{room.aliases?.join(', ')}</h3>
+      <h1 className="text-xl">{room.label}</h1>
+      <h2 className="text-secondary-text">{room.aliases?.join(', ')}</h2>
     </header>
-    {(expanded && room.description) && <div className="pt-2 max-h-72 overflow-y-auto prose prose-default max-w-none"><Markdown>{room.description}</Markdown></div>}
+    {(expanded && room.description) && <div className="pt-2 max-h-72 overflow-y-auto prose prose-default max-w-none">
+      <Markdown
+        components={{
+          h1: 'h3',
+          h2: 'h4',
+          h3: 'h5',
+          h4: 'h6',
+          h5: 'h6',
+        }}
+      >{room.description}</Markdown>
+    </div>}
   </div>
 }
